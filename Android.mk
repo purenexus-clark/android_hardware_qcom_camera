33bd57eef89d78263f63cd276a08ee3c113d15ab
nikhil93uf <nikhil.jan93@ufl.edu>
2016-03-03 00:05:51 +0530

    Android.mk: Force include sub-directories when USE_DEVICE_SPECIFIC_CAMERA

--- Android.mk
+++ Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-camera))
-
 ifneq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
 ifneq ($(filter msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
   ifneq ($(strip $(USE_CAMERA_STUB)),true)
@@ -8,6 +6,4 @@ ifneq ($(filter msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
     endif
   endif
 endif
-endif
-
 endif
